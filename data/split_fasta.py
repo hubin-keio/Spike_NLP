f766f51d4a28ce906c46d3f4274f832822b8bdc6
Bin HU <8225406+hubin-keio@users.noreply.github.com>
2023-05-06 09:56:29 -0600

    Added code to randomly split fasta file to test and train set

---
+++ data/split_fasta.py
@@ -0,0 +1,36 @@
+"""Randomly split a fasta file into two files"""
+
+import os
+import random
+from Bio import SeqIO
+
+def split_fasta(input_fa: str, train_fa: str, test_fa: str):
+
+    train_count = 0
+    test_count = 0
+    total_count = 0
+
+    input_records = list(SeqIO.parse(input_fa, 'fasta'))
+    random.shuffle(input_records)
+
+    with open(train_fa, 'w') as ft, open(test_fa, 'w') as fv:
+
+        for record in input_records:
+            rnd = random.random()
+            if rnd > 0.2:
+                SeqIO.write(record, ft, 'fasta')
+                train_count += 1
+            else:
+                SeqIO.write(record, fv, 'fasta')
+                test_count += 1
+            total_count += 1
+    print(f'Total: {total_count}, Train: {train_count}, Test: {test_count}')
+
+
+if __name__ == '__main__':
+    root_dir = os.path.dirname(__file__)
+    input_fa = os.path.join(root_dir, 'spikeprot0203.clean.uniq.noX.fasta')
+    train_fa = input_fa.replace('.fasta', '_train.fasta')
+    test_fa  = input_fa.replace('.fasta', '_test.fasta')
+
+    split_fasta(input_fa, train_fa, test_fa)
